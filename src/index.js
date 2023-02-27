6918a6567f4c3f3fbd1cea8593723e6dd1128ad8
vikas148 <vb334531@gmail.com>
2023-02-27 21:41:56 +0530

    react app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var React = require("react");
+var ReactDom = require("react-dom");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<h1>Hello react</h1>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDom.render('kya dikhana hai ','kaha dikhana hai','call-back function');
+ReactDom.render(<h1>Hello world !!</h1>,document.getElementById('root'));
